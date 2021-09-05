2cbe3e13276abaa88c675e122d69ff94526714fc
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-09-05 14:12:59 +0300

    Add initial infrastructure

---
+++ lib/infrastructure-stack.ts
@@ -0,0 +1,51 @@
+import * as cdk from '@aws-cdk/core';
+import * as apigw from '@aws-cdk/aws-apigatewayv2';
+import {CorsHttpMethod} from '@aws-cdk/aws-apigatewayv2';
+import * as route53 from '@aws-cdk/aws-route53';
+import * as targets from '@aws-cdk/aws-route53-targets';
+import * as certificateManager from '@aws-cdk/aws-certificatemanager';
+
+const rootDomain = 'picopic.io';
+const apiDomain = `api.${rootDomain}`;
+
+export class InfrastructureStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const hostedZone = route53.HostedZone.fromLookup(this, 'Zone', {
+      domainName: rootDomain,
+    });
+
+    const certificate = new certificateManager.DnsValidatedCertificate(this, 'PicopicApiCertificate', {
+      domainName: apiDomain,
+      hostedZone,
+    });
+
+    const apiDomainName = new apigw.DomainName(this, 'PicopicApiDomainName', {
+      certificate,
+      domainName: apiDomain
+    });
+
+    new apigw.HttpApi(this, 'PicopicHttpApi', {
+      apiName: 'Picopic API',
+      corsPreflight: {
+        allowOrigins: ["*"],
+        allowMethods: [CorsHttpMethod.GET],
+      },
+      defaultDomainMapping: {
+        domainName: apiDomainName,
+      }
+    });
+
+    new route53.ARecord(this, 'PicopicHttpApiARecord', {
+      recordName: apiDomain,
+      zone: hostedZone,
+      target: route53.RecordTarget.fromAlias(
+          new targets.ApiGatewayv2DomainProperties(
+              apiDomainName.regionalDomainName,
+              apiDomainName.regionalHostedZoneId,
+          )
+      ),
+    });
+  }
+}
